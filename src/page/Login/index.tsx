d96c7a88bf4b8b970c65365199c38a344140a0c7
Yeongin Park <pyi7628@gmail.com>
2021-08-03 16:39:36 +0900

    feat: Add Login template

--- src/page/Login/index.tsx
+++ src/page/Login/index.tsx
@@ -1,6 +1,96 @@
 import React, { useState, useEffect } from "react";
+import {
+  Avatar,
+  CssBaseline,
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  makeStyles,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function Login() {
-  return <div>Login page</div>;
+  const classes = useStyles();
+
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(e) => {
+              setId(`${e.target.value}`);
+            }}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => {
+              setPw(`${e.target.value}`);
+            }}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+          >
+            Sign In
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
 }
 export default Login;
