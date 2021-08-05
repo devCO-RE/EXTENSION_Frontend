fbc6d3a16e5bba0f958b7ca0aa0b2e0d0b622930
Yeongin Park <pyi7628@gmail.com>
2021-08-06 03:17:03 +0900

    feat: Make user menu bar

--- src/components/basis/Header/index.tsx
+++ src/components/basis/Header/index.tsx
@@ -6,12 +6,52 @@ import {
   Typography,
   makeStyles,
   createStyles,
+  withStyles,
+  MenuItem,
+  Menu,
+  MenuProps,
+  ListItemText,
+  ListItemIcon,
   Theme,
 } from "@material-ui/core";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
 import { getUserInfo } from "../../../utils";
 
 import "./style.css";
+
+const StyledMenu = withStyles({
+  paper: {
+    border: "1px solid #d3d4d5",
+  },
+})((props: MenuProps) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "center",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center",
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    "&:focus": {
+      backgroundColor: theme.palette.primary.main,
+      "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -35,6 +75,17 @@ const useStyles = makeStyles((theme: Theme) =>
 
 function Header() {
   const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div className={classes.root}>
       <AppBar className={classes.bar} position="static">
@@ -42,10 +93,36 @@ function Header() {
           <Typography variant="h6" className={classes.title}>
             Header
           </Typography>
-          <Button className={classes.userButton} color="inherit">
+          <Button className={classes.userButton} color="inherit" onClick={handleClick}>
             {getUserInfo()} 님
             <AccountCircleIcon className={classes.userIcon} fontSize="large" />
           </Button>
+          <StyledMenu
+            id="customized-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <StyledMenuItem>
+              <ListItemIcon>
+                <SendIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Sent mail" />
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <ListItemIcon>
+                <DraftsIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <ListItemIcon>
+                <InboxIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </StyledMenuItem>
+          </StyledMenu>
         </Toolbar>
       </AppBar>
     </div>
--- src/page/Main/index.tsx
+++ src/page/Main/index.tsx
@@ -4,6 +4,7 @@ function Main() {
   return (
     <div>
       <Header />
+      Main page
     </div>
   );
 }
