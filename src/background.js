43016f13ddf81f285616957928501dac71e189fd
Yeongin Park <pyi7628@gmail.com>
2021-08-13 21:55:24 +0900

    feat: Add webpack configure

---
+++ src/background.js
@@ -0,0 +1,21 @@
+const test = () => {
+    alert("!!!!")
+    var queryInfo = {
+
+        active: true,
+    
+        currentWindow: true
+    
+      };
+    /* eslint-disable no-undef */
+    chrome.tabs.query(queryInfo, function(tabs) {
+
+        var tab = tabs[0];
+        var url = tab.url;
+    
+        alert(url);
+    
+      });
+    
+};
+export default test;
